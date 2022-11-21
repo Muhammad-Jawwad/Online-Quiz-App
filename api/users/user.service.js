1a1b92a192f0e0085554217e794f993436a7b160
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2022-11-21 15:22:19 +0500

    Update user.service.js

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -2,54 +2,53 @@ const pool = require("../../config/database");
 
 module.exports = {
   create: (data, callBack) => {
-    console.log(data,'sajdain');
-    console.log("\n Some Error is ocurred here");
-    // console.log(data);
-    
+    console.log(data);
     pool.query(
-      `insert into msgTable(name, email, subject, message) 
-            values(?,?,?,?)`,
+      `insert into registration(firstName, lastName, gender, email, password, number) 
+            values(?,?,?,?,?,?)`,
       [
-        data.name,
+        data.firstName,
+        data.lastName,
+        data.gender,
         data.email,
-        data.subject,
-        data.message
+        data.password,
+        data.number
       ],
       (error, results, fields) => {
         if (error) {
           callBack(error);
         }
-        return callBack(null, data);
+        return callBack(null, results);
       }
     );
   },
-  // getUserByUserEmail: (email, callBack) => {
-  //   pool.query(
-  //     `select * from msgTable where email = ?`,
-  //     [email],
-  //     (error, results, fields) => {
-  //       if (error) {
-  //         callBack(error);
-  //       }
-  //       return callBack(null, results[0]);
-  //     }
-  //   );
-  // },
-  // getUserByUserId: (id, callBack) => {
-  //   pool.query(
-  //     `select id,name,email,subject,message from msgTable where id = ?`,
-  //     [id],
-  //     (error, results, fields) => {
-  //       if (error) {
-  //         callBack(error);
-  //       }
-  //       return callBack(null, results[0]);
-  //     }
-  //   );
-  // },
+  getUserByUserEmail: (email, callBack) => {
+    pool.query(
+      `select * from registration where email = ?`,
+      [email],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  getUserByUserId: (id, callBack) => {
+    pool.query(
+      `select id,firstName,lastName,gender,email,number from registration where id = ?`,
+      [id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
   getUsers: callBack => {
     pool.query(
-      `select id,name,email,subject,message from msgTable`,
+      `select id,firstName,lastName,gender,email,number from registration`,
       [],
       (error, results, fields) => {
         if (error) {
@@ -58,34 +57,52 @@ module.exports = {
         return callBack(null, results);
       }
     );
+  },
+  updateUser: (data, callBack) => {
+    pool.query(
+      `update registration set firstName=?, lastName=?, gender=?, email=?, password=?, number=? where id = ?`,
+      [
+        data.first_name,
+        data.last_name,
+        data.gender,
+        data.email,
+        data.password,
+        data.number,
+        data.id
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  deleteUser: (data, callBack) => {
+    pool.query(
+      `delete from registration where id = ?`,
+      [data.id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
   }
-  // updateUser: (data, callBack) => {
+  // upload: (data, callBack) => {
+  //   console.log(data);
   //   pool.query(
-  //     `update msgTable set name=?, email=?, subject=?, message=? where id = ?`,
+  //     `insert into registration(fileName) 
+  //           values(?)`,
   //     [
-  //       data.name,
-  //       data.email,
-  //       data.subject,
-  //       data.message,
-  //       data.id
+  //       data.fileName,
   //     ],
   //     (error, results, fields) => {
   //       if (error) {
   //         callBack(error);
   //       }
-  //       return callBack(null, results[0]);
-  //     }
-  //   );
-  // },
-  // deleteUser: (data, callBack) => {
-  //   pool.query(
-  //     `delete from msgTable where id = ?`,
-  //     [data.id],
-  //     (error, results, fields) => {
-  //       if (error) {
-  //         callBack(error);
-  //       }
-  //       return callBack(null, results[0]);
+  //       return callBack(null, results);
   //     }
   //   );
   // }
