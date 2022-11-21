a75e64b6e5779d6f649eeef79db0990b12022988
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2022-11-21 15:24:02 +0500

    Update user.controller.js

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,140 +1,137 @@
 const {
-  create,
-  // getUserByUserEmail,
-  // getUserByUserId,
-  getUsers
-  // updateUser,
-  // deleteUser
+    create,
+    getUserByUserEmail,
+    getCategories,
+    getCategoryByID,
+    createCategory,
+    getQuizByCategoryId
 } = require("./user.service");
 
-const { hashSync, genSaltSync } = require("bcrypt");
-  
 
-// const { sign } = require("jsonwebtoken");
-  
+const { hashSync, genSaltSync, compareSync } = require("bcrypt");
+
+const { sign } = require("jsonwebtoken");
+
 module.exports = {
-  createUser: (req, res) => {
-      const body = req.body;
-      console.log(req,"sajdain No error occur");
-      // const salt = genSaltSync(10);
-      // body.password = hashSync(body.password,salt);
-      create(body, (err, results) => {
-        console.log(results);
-        if (err) {
-          console.log(err);
-          return res.status(500).json({
-            success: 0,
-            message: "Database connection error",
-            message: err
+    createUser: (req, res) => {
+        const body = req.body;
+        const salt = genSaltSync(10);
+        body.password = hashSync(body.password, salt);
+        create(body, (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    success: 0,
+                    message: "Database connection error",
+                    message: err
+                });
+            }
+            return res.status(200).json({
+                success: 1,
+                data: results
+            });
+        });
+    },
+    createCategory: (req, res) => {
+        const body = req.body;
+        console.log(req,"No error occur");
+        createCategory(body, (err, results) => {
+          console.log(results);
+          if (err) {
+            console.log(err);
+            return res.status(500).json({
+              success: 0,
+              message: "Database connection error",
+              message: err
+            });
+          }
+          return res.status(200).json({
+            success: 1,
+            data: results
           });
-        }
-        return res.status(200).json({
-          success: 1,
-          data: results
         });
-      });
-  },
-    // getUserByUserId: (req, res) => {
-    //   const id = req.params.id;
-    //   getUserByUserId(id, (err, results) => {
-    //     if (err) {
-    //       console.log(err);
-    //       return;
-    //     }
-    //     if (!results) {
-    //       return res.json({
-    //         success: 0,
-    //         message: "Record not Found"
-    //       });
-    //     }
-    //     results.password = undefined;
-    //     return res.json({
-    //       success: 1,
-    //       data: results
-    //     });
-    //   });
-    // },
-    getUsers: (req, res) => {
-      getUsers((err, results) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        return res.json({
-          success: 1,
-          data: results
+    },
+    getCategories: (req, res) => {
+        getCategories((err, results) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          return res.json({
+            success: 1,
+            data: results
+          });
         });
-      });
+      },
+      getCategoryByID: (req, res) => {
+        const id = req.params.id;
+        getCategoryByID(id, (err, results) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          if (!results) {
+            return res.json({
+              success: 0,
+              message: "Record not Found"
+            });
+          }
+          results.password = undefined;
+          return res.json({
+            success: 1,
+            data: results
+          });
+        });
+    },
+    getQuizByCategoryId: (req, res) => {
+        const id = req.params.id;
+        getQuizByCategoryId(id, (err, results) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          if (!results) {
+            return res.json({
+              success: 0,
+              message: "Record not Found"
+            });
+          }
+          results.password = undefined;
+          return res.json({
+            success: 1,
+            data: results
+          });
+        });
+      },
+    login: (req, res) => {
+        const body = req.body;
+        getUserByUserEmail(body.email_id, (err, results) => {
+            if (err) {
+                console.log(err);
+            }
+            if (!results) {
+                return res.json({
+                    success: 0,
+                    data: "Invalid email or password"
+                });
+            }
+            const result = compareSync(body.password, results.password);
+            if (result) {
+                results.password = undefined;
+                const jsontoken = sign({ result: results }, "qwe1234", {
+                    expiresIn: "1h"
+                });
+                return res.json({
+                    success: 1,
+                    message: "login successfully",
+                    token: jsontoken
+                });
+            } else {
+                return res.json({
+                    success: 0,
+                    data: "Invalid email or password"
+                });
+            }
+        });
     }
-    // updateUsers: (req, res) => {
-    //   const body = req.body;
-    //   const salt = genSaltSync(10);
-    //   body.password = hashSync(body.password, salt);
-    //   updateUser(body, (err, results) => {
-    //     if (err) {
-    //       console.log(err);
-    //       return false;
-    //     }
-    //     if (!results){
-    //       return req.json({
-    //         success: 0,
-    //         message: "Failed to update user"
-    //       });
-    //     }
-    //     return res.json({
-    //       success: 1,
-    //       message: "updated successfully"
-    //     });
-    //   });
-    // },
-    // deleteUser: (req, res) => {
-    //   const data = req.body;
-    //   deleteUser(data, (err, results) => {
-    //     if (err) {
-    //       console.log(err);
-    //       return;
-    //     }
-    //     if (!results) {
-    //       return res.json({
-    //         success: 0,
-    //         message: "Record Not Found"
-    //       });
-    //     }
-    //     return res.json({
-    //       success: 1,
-    //       message: "user deleted successfully"
-    //     });
-    //   });
-    // }
-    // login: (req, res) => {
-    //   const body = req.body;
-    //   getUserByUserEmail(body.email, (err, results) => {
-    //     if (err) {
-    //       console.log(err);
-    //     }
-    //     if (!results) {
-    //       return res.json({
-    //         success: 0,
-    //         data: "Invalid email or password"
-    //       });
-    //     }
-    //     const result = compareSync(body.password, results.password);
-    //     if (result) {
-    //       results.password = undefined;
-    //       const jsontoken = sign({ result: results }, "qwe1234", {
-    //         expiresIn: "1h"
-    //       });
-    //       return res.json({
-    //         success: 1,
-    //         message: "login successfully",
-    //         token: jsontoken
-    //       });
-    //     } else {
-    //       return res.json({
-    //         success: 0,
-    //         data: "Invalid email or password"
-    //       });
-    //     }
-    //   });
-    // }
-  };
+}
