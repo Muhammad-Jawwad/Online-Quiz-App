f847779bca8815cb53a9b4c5f062ef59079c9862
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2022-11-27 00:47:45 +0500

    Update user.service.js

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -4,13 +4,14 @@ module.exports = {
     create: (data, callBack) => {
         console.log(data);
         pool.query(
-            `insert into register_table(name, email_id, password, mobile_number) 
-              values(?,?,?,?)`,
+            `insert into register_table(name, email_id, password, mobile_number, profile_picture) 
+              values(?,?,?,?,?)`,
             [
                 data.name,
                 data.email_id,
                 data.password,
-                data.mobile_number
+                data.mobile_number,
+                "https://avatars.dicebear.com/api/identicon/" + data.name + ".svg"
             ],
             (error, results, fields) => {
                 if (error) {
@@ -38,9 +39,61 @@ module.exports = {
             }
         );
     },
+    addQuestion: (data, callBack) => {
+        console.log(data);
+        pool.query(
+            `insert into quiz_questions(quiz_id,question,option_1,option_2,option_3,option_4,correct_option) 
+              values(?,?,?,?,?,?,?)`,
+            [
+                data.quiz_id,
+                data.question,
+                data.option_1,
+                data.option_2,
+                data.option_3,
+                data.option_4,
+                data.correct_option
+            ],
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+                return callBack(null, results);
+            }
+        );
+    },
+    addInAttempted: (data, callBack) => {
+        console.log(data);
+        pool.query(
+            `insert into attempted_questions(question_id,user_id,entered_option) 
+              values(?,?,?)`,
+            [
+                data.question_id,
+                data.user_id,
+                data.entered_option
+            ],
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+                return callBack(null, results);
+            }
+        );
+    },
+    getQuestionByQuizId: (id, callBack) => {
+        pool.query(
+            `select question,option_1,option_2,option_3,option_4 from quiz_questions where quiz_id = ? and id NOT IN (SELECT question_id FROM attempted_questions) ORDER BY RAND() LIMIT 1`,
+            [id],
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+                return callBack(null, results[0]);
+            }
+        );
+    },
     getCategoryByID: (id, callBack) => {
         pool.query(
-            `select category_id,category_name,category_picture,no_of_quiz from quiz_categories where category_id = ?`,
+            `select id,category_name,category_picture,no_of_quiz from quiz_categories where id = ?`,
             [id],
             (error, results, fields) => {
                 if (error) {
@@ -52,7 +105,7 @@ module.exports = {
     },
     getCategories: callBack => {
         pool.query(
-            `select category_id,category_name,category_picture,no_of_quiz from quiz_categories`,
+            `select id,category_name,category_picture,no_of_quiz from quiz_categories`,
             [],
             (error, results, fields) => {
                 if (error) {
@@ -64,7 +117,7 @@ module.exports = {
     },
     getQuizByCategoryId: (id, callBack) => {
         pool.query(
-            `select quiz_no,quiz_name,description from quiz_by_category where category_id = ?`,
+            `select quiz_no,picture,quiz_name,no_of_questions,description from quiz_by_category where category_id = ?`,
             [id],
             (error, results, fields) => {
                 if (error) {
@@ -78,6 +131,18 @@ module.exports = {
         pool.query(
             `select * from register_table where email_id = ?`,
             [email],
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+                return callBack(null, results[0]);
+            }
+        );
+    },
+    getQuestionById: (question_id, callBack) => {
+        pool.query(
+            `select * from quiz_questions where id = ?`,
+            [question_id],
             (error, results, fields) => {
                 if (error) {
                     callBack(error);
