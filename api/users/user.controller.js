94ef85c570962ad2be63093cc9820eedd2b1160c
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2022-12-28 02:05:52 +0500

    Update user.controller.js

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -10,17 +10,26 @@ const {
   getQuestionById,
   addInAttempted,
   quizAttempted,
+  updateStatus,
+  quizStatus,
   scoreByQuizId,
   attemptedQuizByUserId,
-  updateUser
+  updateUser,
+  searchCategory
 } = require("./user.service");
 
 const { hashSync, genSaltSync, compareSync } = require("bcrypt");
-
 const { sign } = require("jsonwebtoken");
 
 module.exports = {
   createUser: (req, res) => {
+    /**
+     * Body Require:
+     * name
+     * email_id
+     * password
+     * mobile_number
+     */
     const body = req.body;
     const salt = genSaltSync(10);
     body.password = hashSync(body.password, salt);
@@ -53,7 +62,7 @@ module.exports = {
         console.log(err);
         return false;
       }
-      if (!results) {
+      if (!results.length) {
         return res.json({
           code: 400,
           status: false,
@@ -70,8 +79,13 @@ module.exports = {
     });
   },
   createCategory: (req, res) => {
+    /**
+     * Body Require:
+     * category_name
+     * category_picture
+     * no_of_quiz
+     */
     const body = req.body;
-    console.log(req, "No error occur");
     createCategory(body, (err, results) => {
       console.log(results);
       if (err) {
@@ -106,8 +120,13 @@ module.exports = {
     });
   },
   getCategoryByID: (req, res) => {
-    const id = req.params.id;
-    getCategoryByID(id, (err, results) => {
+    /**
+     * Body Require:
+     * User Id
+     * Category id
+     */
+    const body = req.body;
+    getCategoryByID(body.category_id, (err, results) => {
       if (err) {
         console.log(err);
         return;
@@ -116,11 +135,43 @@ module.exports = {
         return res.json({
           code: 400,
           status: false,
+          message: "Category not found at given id",
+          data: []
+        });
+      }
+      // results.password = undefined;
+      return res.json({
+        code: 200,
+        status: true,
+        message: "The category at a particular Id",
+        data: results
+      });
+    });
+  },
+  searchCategory: (req, res) => {
+    /**
+     * Body Require:
+     * category_name
+     * user_id
+     */
+    const body = req.body
+    const name = body.category_name;
+    console.log(name);
+    searchCategory(name, (err, results) => {
+      console.log("The result is: ", results);
+      if (err) {
+        console.log(err);
+        return false;
+      }
+      if (!results.length) {
+        return res.json({
+          code: 400,
+          status: false,
           message: "Data not found",
           data: []
         });
       }
-      results.password = undefined;
+      // results.password = undefined;
       return res.json({
         code: 200,
         status: true,
@@ -130,8 +181,13 @@ module.exports = {
     });
   },
   getQuizByCategoryId: (req, res) => {
-    const id = req.params.id;
-    getQuizByCategoryId(id, (err, results) => {
+    /**
+     * Body Require:
+     * user_id
+     * category_id
+     */
+    const body = req.body;
+    getQuizByCategoryId(body.category_id, (err, results) => {
       if (err) {
         console.log(err);
         return;
@@ -140,15 +196,15 @@ module.exports = {
         return res.json({
           code: 400,
           status: false,
-          message: "Data not found",
+          message: "Quiz by category id not found",
           data: []
         });
       }
-      results.password = undefined;
+      // results.password = undefined;
       return res.json({
         code: 200,
         status: true,
-        message: "Data found",
+        message: "Quiz by category id found",
         data: results
       });
     });
@@ -176,100 +232,64 @@ module.exports = {
     });
   },
   getQuestionByQuizId: (req, res) => {
-    const id = req.params.id;
-    getQuestionByQuizId(id, (err, results) => {
+    /**
+     * Body requires:
+     * user_id
+     * quiz_id
+     */
+    const body = req.body;
+    // console.log(id);
+    getQuestionByQuizId(body, (err, results) => {
       if (err) {
         console.log(err);
         return;
       }
-      //There is some issue to be resolved!
+      console.log(!results);
       if (!results) {
         console.log("No Question found");
-        quizAttempted(id, (err, results) => {
+        return res.json({
+          code: 400,
+          status: false,
+          message: "First Question not found",
+          data: []
+        });
+      }
+      else {
+        console.log("This is from get question", results)
+        quizStatus(body, (err, result) => {
+          //One user can start the same quiz again
           if (err) {
             console.log(err);
+            return;
+          }
+          console.log("The result at quiz status is: ", result);
+          if (!result) {
+            console.log("Unable to insert in quiz completed");
             return res.json({
               code: 400,
               status: false,
-              message: "Data not found",
+              message: "Unable to insert in quiz completed",
               data: []
             });
           }
-        });
-        return res.json({
-          code: 400,
-          status: false,
-          message: "Data not found",
-          data: []
-        });
-      }
-
-      // results.password = undefined;
-      return res.json({
-        code: 200,
-        status: true,
-        message: "Data found",
-        data: results
-      });
-    });
-  },
-  userAnswer: (req, res) => {
-    let result = false;
-    const body = req.body;
-    getQuestionById(body.question_id, (err, results) => {
-      if (err) {
-        console.log(err);
-      }
-      if (!results) {
-        return res.json({
-          code: 400,
-          status: false,
-          message: "Data not found",
-          data: []
-        });
-      }
-      console.log(results);
-
-      if (results.correct_option == body.entered_option) {
-        result = true;
-      }
-
-      addInAttempted(body, result, (err, results) => {
-        if (err) {
-          console.log(err);
-          return res.json({
-            code: 400,
-            status: false,
-            message: "Data not found",
-            data: []
-          });
-        }
-      });
-
-      if (result) {
-        results.correct_option = undefined;
-        const jsontoken = sign({ result: results }, "qwe1234", {
-          expiresIn: "1h"
+          console.log("Inserted in quiz_complete")
         });
         return res.json({
           code: 200,
           status: true,
-          message: "Data found",
-          data: jsontoken
-        });
-      } else {
-        return res.json({
-          code: 400,
-          status: false,
-          message: "Data not found",
-          data: []
+          message: "First question found",
+          data: results
         });
       }
     });
   },
   attemptedQuizByUserId: (req, res) => {
-    const id = req.params.id;
-    attemptedQuizByUserId(id, (err, results) => {
+    /**
+     * Body Require:
+     * User id
+     */
+    const body = req.body;
+    attemptedQuizByUserId(body.user_id, (err, results) => {
       if (err) {
         console.log(err);
         return;
@@ -291,8 +311,13 @@ module.exports = {
     });
   },
   scoreByQuizId: (req, res) => {
-    const id = req.params.id;
-    scoreByQuizId(id, (err, results) => {
+    /**
+     * Body requires:
+     * User id
+     * Quiz id
+     */
+    const body = req.body;
+    scoreByQuizId(body, (err, results) => {
       if (err) {
         console.log(err);
         return;
@@ -301,14 +326,14 @@ module.exports = {
         return res.json({
           code: 400,
           status: false,
-          message: "Data not found",
+          message: "Score not calculated",
           data: []
         });
       }
       return res.json({
         code: 200,
         status: true,
-        message: "Data found",
+        message: "The score has found successfully",
         data: results
       });
     });
@@ -318,12 +343,13 @@ module.exports = {
     getUserByUserEmail(body.email_id, (err, results) => {
       if (err) {
         console.log(err);
+        return false;
       }
       if (!results) {
         return res.json({
           code: 400,
           status: false,
-          message: "Data not found",
+          message: "Invalid email",
           data: []
         });
       }
@@ -336,7 +362,7 @@ module.exports = {
         return res.json({
           code: 200,
           status: true,
-          message: "Data found",
+          message: "Successfully Login",
           // data: jsontoken
           data: results
         });
@@ -344,8 +370,130 @@ module.exports = {
         return res.json({
           code: 400,
           status: false,
-          message: "Data not found",
+          message: "Invalid email id or password",
           data: []
+        });
+      }
+    });
+  },
+  userAnswer: (req, res) => {
+    /**
+     * Body requires:
+     * User id
+     * Quiz id
+     * Question id
+     * Entered option
+     */
+    const body = req.body;
+    var check = false;
+    var msg = "Sorry! Your answer is incorrect!";
+    getQuestionById(body.question_id, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(!results);
+      if (!results) {
+        return res.json({
+          code: 400,
+          status: false,
+          message: "Question you want to answer not found",
+          data: []
+        });
+      }
+      console.log("The previous question that answered: ", results);
+      //Checking answer
+      if (results.correct_option == body.entered_option) {
+        check = true;
+        msg = "Congratulations! Your answer is correct"
+        console.log("Answer Status", check);
+      }
+      //Adding the question into attempted question
+      addInAttempted(body, check, (err, results) => {
+        if (err) {
+          console.log(err);
+          return res.json({
+            code: 400,
+            status: false,
+            message: "Data not added to attempted question table",
+            data: []
+          });
+        }
+      });
+      console.log("The question added in attempted question")
+    });
+    //Calling next question
+    getQuestionByQuizId(body, (err, next_results) => {
+      if (err) {
+        console.log("Next Question finding error: ", err);
+        return;
+      }
+      console.log(!next_results);
+      if (!next_results) {
+        console.log("No Question found");
+        //When there is no question left in quiz we need to calculate it's score
+        quizAttempted(body, (err, results) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log("From quizAttempted fun: ", !results);
+          if (!results) {
+            return res.json({
+              code: 400,
+              status: false,
+              message: msg,
+              data: "Score not calculated"
+            });
+          }
+          // Now getting the calculated score from quiz_attempted
+          scoreByQuizId(body, (err, results) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log("From scoreByQuizId fun: ", !results);
+            if (!results) {
+              return res.json({
+                code: 400,
+                status: false,
+                message: msg,
+                data: "Score not found"
+              });
+            }
+            //update status in quiz completed table
+            updateStatus(body, (err, results) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
+              console.log("From updateStatus fun: ", !results);
+              if (!results) {
+                return res.json({
+                  code: 400,
+                  status: false,
+                  message: msg,
+                  data: "Status not Updated"
+                });
+              }
+            });
+            return res.json({
+              code: 200,
+              status: true,
+              message: msg,
+              data: "The quiz is ended",
+              score: results
+            });
+          });
+        });
+      }
+      else {
+        console.log("The next question after answer", next_results);
+        return res.json({
+          code: 200,
+          status: true,
+          message: msg,
+          data: next_results
         });
       }
     });
