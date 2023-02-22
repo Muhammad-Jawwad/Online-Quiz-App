b3f89a92dfc8863358ad56fc0c0bcbc718cb5d7a
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2023-02-22 18:15:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express();
-const userRouter = require("./api/users/user.router");
 const bodyParser = require('body-parser');
+const logger = require("morgan");
+const userRouter = require("./api/users/user.router");
 
-var cors = require('cors')
-app.use(cors())
+const app = express();
 
 app.use(express.json());
-
+app.use(bodyParser.json());
+/*The Implementation for the middleware*/
+app.use((req, res, next) => {
+    console.log("Middleware is calling:", "\nRequest Method -> ", req.method, "\nRequest IP -> ", req.ip, "\nRequest Path -> ", req.path);
+    next();
+});
+app.use(logger());
 app.use("/api/users", userRouter);
 
+
 app.listen(process.env.APP_PORT, () => {
-  console.log("server up and running on PORT :", process.env.APP_PORT);
+    console.log("server up and running on PORT :", process.env.APP_PORT);
 });
