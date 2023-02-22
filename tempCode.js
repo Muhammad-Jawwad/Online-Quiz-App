6d2f16f890d86d22ae65290691a8a6b0e98dab93
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2023-02-22 18:15:57 +0500

    Update tempCode.js

--- tempCode.js
+++ tempCode.js
@@ -25,3 +25,59 @@ app.get('/', (req, res) => {
 //Used  in app.js
 var cors = require('cors')
 app.use(cors())
+
+
+//API 
+fetchData: (req, res) => {
+    /**
+     * Body Require:
+     * id as end-point
+     */
+    const id = req.params.id;
+    fetchData(id, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (!results) {
+        return res.json({
+          code: 400,
+          status: false,
+          message: "Error fetching data from database",
+          data: []
+        });
+      } else {
+
+        const arr = Object.entries(results).map(([key, value]) => ({ name: key, value }));
+
+        console.log(arr);
+        // let userData = Array.from(arr);
+        console.log("The object is an array", Array.isArray(arr));
+        console.log("There is no error in data fetching", results);
+
+        // THis method is working
+        const convertJsonToExcel = () => {
+
+          const workSheet = XLSX.utils.json_to_sheet(arr);
+          const workBook = XLSX.utils.book_new();
+
+          XLSX.utils.book_append_sheet(workBook, workSheet, "arr")
+          // Generate buffer
+          XLSX.write(workBook, { bookType: 'xlsx', type: "buffer" })
+
+          // Binary string
+          XLSX.write(workBook, { bookType: "xlsx", type: "binary" })
+
+          XLSX.writeFile(workBook, "usersData.xlsx")
+
+        }
+        convertJsonToExcel()
+        return res.json({
+          code: 200,
+          status: true,
+          message: "The category at a particular Id",
+          data: results
+        });
+      }
+    });
+  },
