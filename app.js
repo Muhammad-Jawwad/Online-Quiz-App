6d19073613dc26068ce5e34ffacce9c470c54ca9
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2023-02-24 11:53:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 require("dotenv").config();
 
+
 const express = require("express");
 const bodyParser = require('body-parser');
-const logger = require("morgan");
 const userRouter = require("./api/users/user.router");
+
 
 const app = express();
 
+
 app.use(express.json());
 app.use(bodyParser.json());
-/*The Implementation for the middleware*/
-app.use((req, res, next) => {
-    console.log("Middleware is calling:", "\nRequest Method -> ", req.method, "\nRequest IP -> ", req.ip, "\nRequest Path -> ", req.path);
-    next();
-});
-app.use(logger());
 app.use("/api/users", userRouter);
 
 
