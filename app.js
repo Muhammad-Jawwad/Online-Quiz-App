797d64ad087f3898fe411d0f52aa91af9550bc35
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2023-03-20 12:58:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require('body-parser');
 const userRouter = require("./api/users/user.router");
+const adminRouter = require("./api/admin/admin.router");
+
 
 
 const app = express();
@@ -12,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use("/api/users", userRouter);
+app.use("/api/admin", adminRouter);
 
 
 app.listen(process.env.APP_PORT, () => {
